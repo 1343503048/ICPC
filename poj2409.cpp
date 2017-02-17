614067c7fb1ae3efa743d012ce541b2dbddb92e9
zq <1343503048@qq.com>
2017-02-17 21:33:51 +0800

    Burnside & Polay定理

---
+++ poj2409.cpp
@@ -0,0 +1,33 @@
+// http://blog.csdn.net/lyc1635566ty/article/details/52545355
+// polay
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a % b);
+} 
+
+int polay_circle(int c, int s) {
+	int ans = 0;
+	// 旋转
+	for(int i = 1; i <= s; ++ i) {	//i为转动几个珠子
+		ans += pow(c, gcd(s, i));
+	}
+	// 翻转
+	if(s & 1) {	//奇数时只存在一种,(穿过一个珠子为对角线)
+		ans += s * pow(c, s<<1|1);
+	}
+	else {	// 偶数时存在两种(分别为转过0个珠子为对角线和穿过两个珠子为对角线)
+		ans += (pow(c, s<<1) + pow(c, s<<1|1)) * s / 2;
+	}
+	return ans / (n * 2);
+}
+
+int main() {
+	int c, s;
+	while(scanf("%d%d", &c, &s) && c != 0) {
+		printf("%d\n", polay_circle(c, s));
+	}
+	return 0;
+}
