8e2fc9edf844c12a2535676a29b434bb591ad502
zq <1343503048@qq.com>
2017-02-18 23:38:22 +0800

    多重背包

--- 2191.cpp
+++ 2191.cpp
@@ -1,24 +1,37 @@
+//多重背包转化为01背包
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 100+5;
+const int N = 500+5;
 
-int dp[N];
+int dp[N], w[N], v[N];
 
 int main(int argc, char const *argv[]) {
 	int t;
 	scanf("%d", &t);
 	while (t --) {
 		memset(dp, 0, sizeof(dp));
-		int n, m, value, weight, cnt;
+		int n, m, value, weight, cnt, pos = 0;
 		scanf("%d%d", &n, &m);
+		
 		for(int i = 0; i < m; ++ i) {
 			scanf("%d%d%d", &value, &weight, &cnt);
-			for(int k = 1; k <= cnt; ++ k) {	//不取的那种情况已经排除掉了，因为dp[j]对应的就是没取的状态  
-				for(int j = n; j >= value; -- j) {
-					dp[j] = max(dp[j], dp[j-value]+weight);
-				}
+			int k = 1;
+			while(k < cnt) {
+				v[pos] = k * value;
+				w[pos++] = k * weight;
+				cnt -= k;
+				k <<= 1;
+			}
+			v[pos] = cnt * value;
+			w[pos++] = cnt * weight;
+		}
+
+		for(int i = 0; i < pos; ++ i) {
+			for(int j = n; j >= v[i]; -- j) {
+				dp[j] = max(dp[j], dp[j-v[i]] + w[i]);
 			}
 		}
+
 		printf("%d\n", dp[n]);
 	}
 	return 0;
