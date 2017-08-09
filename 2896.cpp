77dc4ddd16ce9e338ea300b0884a6fe0ce95fc69
1343503048 <1343503048@qq.com>
2017-08-09 13:30:43 +0800

    AC自动机

--- 2896.cpp
+++ 2896.cpp
@@ -1,129 +1,132 @@
-//c++ 
-//accepted
-//312Ms
 
-#include <iostream>
-#include <vector>
-#include <queue>
+/***********************************************************
+   
+        FileName：2896.cpp
+          Author：zQ
+           Email: zq216991@foxmail.com
+          Create：2017-08-09 10:15:12
+    Descripttion：字典树
+   
+*************************************************************/
+
+#include <cstdio>
 #include <set>
-#include <fstream>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 struct node {
-	int cnt;
-	node *fail, *next[130];//字符很多，注意不能仅用26
+    int cnt;
+    node *fail, *next[130];
 };
 
-node memory[500000];		//动态建树需要释放内存，所以采用静态建树
+node ret[100000];
 char key[205], s[10005];
-node *root;
-int c;
+node *rt;
+int tot;
 
+// 建立字典树
 void insert(int num) {
-	int id, l = strlen(key);
-	node *cur = root;
-	for(int i = 0; i < l; ++ i) {
-		id = (int)key[i];
-		if(cur->next[id] == NULL) {
-			cur->next[id] = &memory[c++];
-		}
-		cur = cur->next[id];
-	}
-	cur->cnt = num;
+    int l = strlen(key);
+    node *cur = rt;
+    for(int i = 0; i < l; ++ i) {
+        int id = key[i];
+        if(cur->next[id] == NULL) {
+            cur->next[id] = &ret[++tot];
+        }
+        cur = cur->next[id];
+    } 
+    cur->cnt = num;
 }
 
+// 
 void build() {
-	queue<node *> Q;
-	Q.push(root);
-	while(!Q.empty()) {
-		node *cur = Q.front();
-		Q.pop();
-		for(int i = 0; i < 130; ++ i) {
-			if(cur->next[i] != NULL){
-				if(cur == root) {
-					cur->next[i]->fail = root;
-				}
-				else {
-					node *temp = cur->fail;
-					while(temp != NULL) {
-						if(temp->next[i] != NULL) {
-							cur->next[i]->fail = temp->next[i];
-							break;
-						}
-						temp = temp->fail;
-					}
-					if(temp == NULL) {
-						cur->next[i]->fail = root;
-					}
-				}
-				Q.push(cur->next[i]);
-			}
-		}
-	}
+    queue<node *> Q;
+    Q.push(rt);
+    while(!Q.empty()) {
+        node *cur = Q.front();
+        Q.pop();
+        for(int i = 0; i < 130; ++ i) {
+            if(cur->next[i] == NULL) continue;
+            if(cur == rt) {
+                cur->next[i]->fail = rt;
+            }
+            else {
+                node *tmp = cur->fail;
+                if(tmp == NULL) {   // 首次遍历
+                    cur->next[i]->fail = rt;
+                }
+                while(tmp != NULL) {
+                    if(tmp->next[i] != NULL) {
+                        cur->next[i]->fail = tmp->next[i];
+                        break;
+                    }
+                    tmp = tmp->fail;
+                }
+            }
+            Q.push(cur->next[i]);
+        }
+    }
 }
 
 int query(int num) {
-	set<int> v;
-	node *cur = root;
-	int id, l = strlen(s);
-	for (int i = 0; i < l; ++ i) {
-		id = (int)s[i];
-		while (cur->next[id] == NULL && cur != root) {
-			cur = cur->fail;
-		}
-		cur = cur->next[id];
-		if(cur == NULL) {
-			cur = root;
-		}
-		node *temp = cur;
-		while(temp != root) {
-			if(temp->cnt > 0) {
-				v.insert(temp->cnt);
-			}
-			temp = temp->fail;
-		}
-	}
-	if(v.size() != 0) {
-		cout << "web " << num << ":";
-		for(set<int>::iterator it = v.begin(); it != v.end(); ++ it) {
-			cout << " " << *it;
-		}
-		cout << endl;
-		return 1;
-	}
-	else {
-		return 0;
-	}
+    set<int> st;
+    node *cur = rt;
+    int l = strlen(s);
+    for(int i = 0; i < l; ++ i) {
+        int id = s[i];
+        while(cur->next[id] == NULL && cur != rt) {
+            cur = cur->fail;
+        }
+        cur = cur->next[id];
+        if(cur == NULL) {
+            cur = rt;
+        }
+        node *tmp = cur;
+        while(tmp != rt) {
+            if(tmp->cnt > 0) {
+                st.insert(tmp->cnt);
+            }
+            tmp = tmp->fail;
+        }
+    }
+    if(st.size() > 0) {
+        printf("web %d:", num);
+        for(auto x : st) {
+            printf(" %d", x);
+        }
+        printf("\n");
+        return 1;
+    }
+    return 0;
 }
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	// ifstream cin("data.in");
-	int n, m;
-	while(cin >> n) {
-		c = 0;
-		root = &memory[c++];
-		int ans = 0;
-		for(int i = 1; i <= n; ++ i) {
-			cin >> key;
-			insert(i);
-		}
-		build();
-		cin >> m;
-		for(int i = 1; i <= m; ++ i) {
-			cin >> s;
-			ans += query(i);
-		}
-		cout << "total: " << ans << endl;
-		for(int i = 0; i < c; ++ i) {
-			memory[i].cnt = 0;
-			memory[i].fail = NULL;
-			for(int i = 0; i < 130; ++ i){
-				memory[i].next[i] = NULL;
-			}
-		}
-	}
-	return 0;
+    int n;
+    while(scanf("%d", &n) != EOF) {
+        tot = 0;
+        rt = &ret[++tot];
+        for(int i = 1; i <= n; ++ i) {
+            scanf("%s", key);
+            insert(i);
+        }
+        build();
+        int m;
+        scanf("%d", &m);
+        int ans = 0;
+        for(int i = 1; i <= m; ++ i) {
+            scanf("%s", s);
+            ans += query(i);
+        }
+
+        printf("total: %d\n", ans);
+        for(int i = 0; i <= tot; ++ i) {
+            ret[i].cnt = 0;
+            ret[i].fail = NULL;
+            for(int j = 0; j < 130; ++ j) {
+                ret[i].next[i] = NULL;
+            }
+        }
+    }
+    return 0;
 }
