f278a302332564a2f85540da9b08e55ba91605ea
1343503048 <1343503048@qq.com>
2017-09-17 22:13:18 +0800

    AC自动机

--- 2222.cpp
+++ 2222.cpp
@@ -1,62 +1,113 @@
-#include <bits/stdc++.h>
+
+/***********************************************************
+   
+        FileName：2222.cpp
+          Author：zQ
+           Email: zq216991@foxmail.com
+          Create：2017-08-09 13:33:17
+    Descripttion：AC自动机
+   
+*************************************************************/
+
+#include <cstring>
+#include <cstdio>
+#include <queue>
 using namespace std;
+const int N = 5e5+5;
 
-struct node {
-	int cnt;
-	node* next[26];
-	node(){
-		cnt = 0;
-		memset(next, 0, sizeof(next));
-	}
-};
+int np[N][26];
+int fail[N];
+int cnt[N];
+char keyword[55];
+char s[1000005];
+int o;
 
-char s[1000005], p[55];
-
-void build(node *root, char *p) {
-	int l = strlen(p), id;
-	node *cur = root;
-	for(int i = 0; i < l; ++ i) {
-		id = p[i] - 'a';
-		if(cur->next[id] == NULL) {
-			cur->next[id] = new node();
-		}
-		cur = cur->next[id];
-	}
-	cur->cnt ++;
+void insert() {
+    int l = strlen(keyword);
+    int cur = 1;
+    for(int i = 0; i < l; ++ i) {
+        int id = keyword[i] - 'a';
+        if(np[cur][id] == 0) {
+            np[cur][id] = ++ o;
+        }
+        cur = np[cur][id];
+    }
+    ++ cnt[cur];
 }
 
-int query(node *root, char *p) {
-	int l = strlen(p);
-	int ans = 0, id;
-	for(int i = 0; i < l; ++ i) {
-		node *cur = root;
-		for(int j = i; j < l; ++ j) {
-			id = p[j] - 'a';
-			cur = cur->next[id];
-			if(cur == NULL) {
-				break;
-			}
-			ans += cur->cnt;
-			cur->cnt = 0;	//notice: clear to 0
-		}
-	}
-	return ans;
+void build() {
+    queue<int> Q;
+    Q.push(1);
+    while(!Q.empty()){
+        int cur = Q.front();
+        Q.pop();
+        for(int i = 0; i < 26; ++ i) {
+            if(np[cur][i] == 0) continue;
+            if(cur == 1) {
+                fail[np[cur][i]] = 1;
+            }
+            else {
+                int tmp = fail[cur];
+                while(tmp != 0) {
+                    if(np[tmp][i] != 0) {
+                        fail[np[cur][i]] = np[tmp][i];
+                        break;
+                    }
+                    tmp = fail[tmp];
+                }
+                if(tmp == 0) {    // 在while之后`
+                    fail[np[cur][i]] = 1;
+                }
+            }
+            Q.push(np[cur][i]);
+        }
+    }
 }
 
-int main(int argc, char const *argv[]) {
-	// freopen("data.in", "r", stdin);
-	int t;
-	scanf("%d", &t);
-	while(t --) {
-		int n;
-		cin >> n;
-		node *root = new node();
-		while(n --) {
-			scanf("%s", p);
-			build(root, p);
-		}
-		scanf("%s", s);
-		cout << query(root, s) << endl;
-	}
-	return 0;
+void query() {
+    int res = 0;
+    int l = strlen(s);
+    int cur = 1;
+    for(int i = 0; i < l; ++ i) {
+        int id = s[i]-'a';
+        while(np[cur][id] == 0 && cur != 1) {
+            cur = fail[cur];
+        }
+        cur = np[cur][id];
+        if(cur == 0) {
+            cur = 1;
+        }
+        int tmp = cur;
+        while(tmp != 1) {
+            res += cnt[tmp];
+            cnt[tmp] = 0;
+            tmp = fail[tmp];
+        }
+    }
+    printf("%d\n", res);
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t --) {
+        memset(np, 0, sizeof(np));
+        memset(fail, 0, sizeof(fail));
+        memset(cnt, 0, sizeof(cnt));
+        int n;
+        scanf("%d", &n);
+        o = 1;
+        while(n --) {
+            scanf("%s", keyword);
+            insert();          
+        }
+
+        build();
+
+        scanf("%s", s);
+
+        query();
+    
+    }
+    return 0;
 }
